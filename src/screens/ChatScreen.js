2d4eb21c20771af2bca273d293a02e8b6852b239
Sonu Kumar <sonu.kumar@teliolabs.com>
2023-07-30 08:18:26 +0530

    input box done

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,6 +1,8 @@
 import {
   FlatList,
   ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   Text,
   View,
@@ -15,15 +17,20 @@ import messages from "../../assets/data/messages.json";
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-            data={messages}
-            renderItem={({ item }) => <Message message={item} />}
-            style={styles.list}
-            inverted
-          />
-          <InputBox />
-    </ImageBackground>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.bg}
+    >
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 const styles = StyleSheet.create({
